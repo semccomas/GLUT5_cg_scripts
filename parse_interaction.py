9c74bb5f747f8d2e7143bc2321c44d619cd65560
semccomas <semccomas@gmail.com>
2020-05-19 14:13:12 +0200

    updates

--- parse_interaction.py
+++ parse_interaction.py
@@ -10,50 +10,60 @@ from cutoff_function import dual_cutoff  #dual cutoff script is elsewhere, see b
 ## since it's r1s1, r2s1, r3s1... r1s2, r2s2, r3s2...
 
 num_residues = 473
-test_df = pd.DataFrame(index = np.arange(num_residues))
-lipid_names = ['POPI', 'POPS', 'CHOL']
+filename = 'GLUT5.O.30'
+final_df = pd.DataFrame(index = np.arange(num_residues))
+if '30' in filename:
+    lipid_names = ['POPC', 'POPE', 'POPS', 'CHOL', 'POPI', 'DPCE']
+else:
+    lipid_names = ['POPC', 'POPE', 'POPS', 'CHOL', 'POPI']
+reps = 3
+lipid_df = pd.DataFrame(index = np.arange(num_residues))
+
 for lipid in lipid_names:
-    longs_total = np.loadtxt('../interactions/GLUT5.I.30.1.%s.6cutoff.xvg' %lipid, dtype = bool)
-    short_total = np.loadtxt('../interactions/GLUT5.I.30.1.%s.4cutoff.xvg' %lipid, dtype = bool)
-    
-    num_lipids = int(np.shape(longs_total)[1]/num_residues)
-    
-    
-    residue_start_index = 0
-    residue_end_index = num_lipids
-    resid_dict = {}  #use this to add to pandas dictionary. Seems easier to add whole dict at end rather than appending to df row by row
-    for resid in range(num_residues): #loop through each residue
-        resid_short = short_total[:,residue_start_index:residue_end_index]
-        resid_long = longs_total[:,residue_start_index:residue_end_index]
+    print(lipid)
+    for rep in np.arange(1,reps+1):
+	print(rep)
+        longs_total = np.loadtxt('../interactions/%s.%i.%s.6cutoff.xvg' %(filename, rep, lipid), dtype = bool)
+        short_total = np.loadtxt('../interactions/%s.%i.%s.4cutoff.xvg' %(filename,rep, lipid), dtype = bool)
         
-        lipid_total_counts = []  ## putting entire array together into a list of counts, so no longer by lipid resid type but just lipid type overall
+        num_lipids = int(np.shape(longs_total)[1]/num_residues)
         
-        ## loop through each lipid per residue
-        for short, longs in zip(resid_short.T, resid_long.T):  #need to transpose because you want to read lipid by lipid, now the x axis is time
-            start_read = 0
-            counts = []
-            #dual_cutoff script can be found in other file. I remove it here for simplicity
-            ## all you need is where to start, and it needs the input of two 1D numpy array or LIST, equal length
-            ### they should be in the dimension of time. (so per function, one lipid binding to one residue or COM
-            ## of protein or whatever). This array should also be boolean
-            ## ex an array looking like this: short = [False, False, True, True, False, True], and long = [True, True, True, True, True, True]
-            ### will give the output of 4!!! (start at first true in short, and then read long counts)  
-            while start_read < len(longs):
-                res_time, trigger_start = dual_cutoff(short,longs,start_read)
-                start_read = res_time + trigger_start  #continue from residence time + wherever you started from again
-                if start_read <= len(longs):
-                    counts.append(res_time)
-            lipid_total_counts.append(counts) # i like to do it this way so I can double check in the original array that things are correct
         
-        ### prepare for DF entry
-        lipid_total_counts = np.hstack(lipid_total_counts) #make everything into one array, no longer arranged by lipid type
-        resid_dict[resid] = lipid_total_counts
+        residue_start_index = 0
+        residue_end_index = num_lipids
+        resid_dict = {}  #use this to add to pandas dictionary. Seems easier to add whole dict at end rather than appending to df row by row
+        for resid in range(num_residues): #loop through each residue
+            resid_short = short_total[:,residue_start_index:residue_end_index]
+            resid_long = longs_total[:,residue_start_index:residue_end_index]
+            
+            lipid_total_counts = []  ## putting entire array together into a list of counts, so no longer by lipid resid type but just lipid type overall
+            
+            ## loop through each lipid per residue
+            for short, longs in zip(resid_short.T, resid_long.T):  #need to transpose because you want to read lipid by lipid, now the x axis is time
+                start_read = 0
+                counts = []
+                #dual_cutoff script can be found in other file. I remove it here for simplicity
+                ## all you need is where to start, and it needs the input of two 1D numpy array or LIST, equal length
+                ### they should be in the dimension of time. (so per function, one lipid binding to one residue or COM
+                ## of protein or whatever). This array should also be boolean
+                ## ex an array looking like this: short = [False, False, True, True, False, True], and long = [True, True, True, True, True, True]
+                ### will give the output of 4!!! (start at first true in short, and then read long counts)  
+                while start_read < len(longs):
+                    res_time, trigger_start = dual_cutoff(short,longs,start_read)
+                    start_read = res_time + trigger_start  #continue from residence time + wherever you started from again
+                    if start_read <= len(longs):
+                        counts.append(res_time)
+                lipid_total_counts.append(counts) # i like to do it this way so I can double check in the original array that things are correct
+            
+            ### prepare for DF entry
+            lipid_total_counts = np.hstack(lipid_total_counts) #make everything into one array, no longer arranged by lipid type
+            resid_dict[resid] = lipid_total_counts
+            
+            # move onto next set of lipids
+            residue_start_index = residue_start_index + num_lipids
+            residue_end_index = residue_end_index + num_lipids
+            
+            ## add to pandas DF!! This will contain all 3 replicas, we will put them together into 1 dataframe, and then add it to the final dataframe
+        lipid_df['%s.%s'%(lipid,rep)] = pd.Series(resid_dict)
         
-        # move onto next set of lipids
-        residue_start_index = residue_start_index + num_lipids
-        residue_end_index = residue_end_index + num_lipids
-        
-    ## add to pandas DF!! 
-    test_df[lipid] = pd.Series(resid_dict)
-    
-    
+lipid_df.to_pickle('%s.interaction_counts.pkl' %filename)
