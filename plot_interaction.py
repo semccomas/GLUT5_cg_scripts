567fbc6212b88d79f6d0907c5bbba954efbbb18e
semccomas <semccomas@gmail.com>
2020-05-29 11:06:44 +0200

    update to plots

--- plot_interaction.py
+++ plot_interaction.py
@@ -11,13 +11,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 num_residues = 473
-filename = 'GLUT5.O.0'
+filename = 'GLUT5.I.0'
 final_df = pd.DataFrame(index = np.arange(num_residues))
 if '30' in filename:
-    lipid_names = ['POPC', 'POPE', 'POPS', 'CHOL', 'POPI', 'DPCE']
+    lipid_names = ['POPC', 'POPE', 'POPS', 'POPI', 'CHOL', 'DPCE']
     #lipid_names = ['POPI']
 else:
-    lipid_names = ['POPC', 'POPE', 'POPS', 'CHOL', 'POPI']
+    lipid_names = ['POPC', 'POPE', 'POPS', 'POPI', 'CHOL']
 reps = 3
 resid_dict = {}
 mean_dict = {}
@@ -37,19 +37,23 @@ for lipid in lipid_names:
         mean_df[lipid] = pd.Series(mean_dict)
 
 mean_df=mean_df.fillna(0) #need to remove NaN's for plotting, just make them zeros
-
+ ## since the original trajectory is every 10ns, one binding event of 150 for example is 1500ns
+ ## I want to represent this in us, therefore *10 and /1000 (== /100) to make 1.5us
+mean_df = mean_df/100
 #### plotting
 
 #plotting per residue
-ind1 = np.arange(0,230)       
-ind2 = np.arange(230,473)       
+start_graph = 230
+ind1 = np.arange(0,start_graph)       
+ind2 = np.arange(start_graph,473)       
 width = 1
 bot1 = 0
 bot2 = 0
 
-fig, (ax1, ax2) = plt.subplots(2,1)
+fig = plt.figure(figsize=(10,5))
+ax1, ax2 = fig.subplots(2,1)
 
-colors = ['red', 'green', 'orange', 'blue', 'purple', 'black']
+colors = ['#FF6B35', '#33658A', '#86BBD8', '#7BAA34', '#F6AE2D', '000000']
 for n, lipid in enumerate(lipid_names):
     ax1.bar(ind1, mean_df.loc[ind1, lipid], width, bottom = bot1, color = colors[n], label = lipid) 
     ax2.bar(ind2, mean_df.loc[ind2, lipid], width, bottom = bot2, color = colors[n], label = lipid)
@@ -57,25 +61,19 @@ for n, lipid in enumerate(lipid_names):
     bot1 = bot1 + mean_df.loc[ind1, lipid]  #increase the bottom every time, so that you stack the lipid types
     bot2 = bot2 + mean_df.loc[ind2, lipid]  #increase the bottom every time, so that you stack the lipid types
 
-'''
-#plotting per residue
-ind = np.arange(473)       
-width = 1
-bot = 0
+ax1.set_xlim(0,start_graph)
+ax2.set_xlim(start_graph, 473)
+ax1.set_ylim(0, 6)
+ax2.set_ylim(0, 6)
 
-mean_df=mean_df.fillna(0)
-colors = ['red', 'green', 'orange', 'blue', 'purple', 'black']
-for n, lipid in enumerate(lipid_names):
-    plt.bar(ind, mean_df.loc[ind, lipid], width, bottom = bot, color = colors[n], label = lipid) 
-    ### this plots along the residue range, calling the mean of that range, and taking the bottom value from before
-    bot = bot + mean_df.loc[ind, lipid]  #increase the bottom every time, so that you stack the lipid types
-    print(bot)
+#ax2.set_xlabel('Residue index')
+#ax2.yaxis.set_label_coords(-0.03,1.5)
+#ax2.set_ylabel('Average residence time (us)')
+#plt.text(-0.03, 1.2, 'Normalized RDF', rotation =90)
 
-'''
-
-plt.legend()
-plt.tight_layout()
-plt.savefig('per_resid_interaction.%s.png' %filename, dpi = 800)
+plt.legend(bbox_to_anchor=(1.2,1.5))
+#plt.tight_layout()
+plt.savefig('per_resid_interaction.%s.png' %filename, dpi = 800, bbox_inches='tight')
 
 
 
